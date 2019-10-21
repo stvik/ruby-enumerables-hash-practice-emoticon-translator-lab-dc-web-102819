b25b7bd11574e3a0aaf2c712d27552e186430ab6
Sara Tarnvik <stvik2@gmail.com>
2019-10-21 13:48:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,16 @@
 # require modules here
 
-def load_library
+def load_library(path)
+  require "yaml"
+  hash = YAML.load_file(path)
+  puts file
   # code goes here
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(english)
   # code goes here
 end
 
-def get_english_meaning
+def get_english_meaning(japanese)
   # code goes here
 end
